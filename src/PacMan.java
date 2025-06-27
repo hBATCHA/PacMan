9b4f886315aea6c967523bfa6b0ee3fc4b26c7be
Hashif Batcha <hashifbatcha@macbook-pro-de-hashif.home>
2025-06-28 01:31:57 +0200

    Étape 5 terminée - Grille du jeu (21x19 tile map)

--- src/PacMan.java
+++ src/PacMan.java
@@ -20,6 +20,31 @@ public class PacMan extends JPanel {
     private Image pacmanLeftImage;
     private Image pacmanRightImage;
 
+    // Carte du jeu (21 lignes x 19 colonnes)
+    private String[] tileMap = {
+            "XXXXXXXXXXXXXXXXXXX",
+            "X        X        X",
+            "X XX XXX X XXX XX X",
+            "X                 X",
+            "X XX X XXXXX X XX X",
+            "X    X   X   X    X",
+            "XXXX XXX X XXX XXXX",
+            "0000 X0000000X 0000",
+            "XXXX X XXrXX X XXXX",
+            "X      XbpoX      X",
+            "XXXX X XXXXX X XXXX",
+            "0000 X0000000X 0000",
+            "XXXX XXX X XXX XXXX",
+            "X    X   X   X    X",
+            "X XX X XXXXX X XX X",
+            "X  X     P     X  X",
+            "XX X X XXXXX X X XX",
+            "X    X   X   X    X",
+            "X XXXXXX X XXXXXX X",
+            "X                 X",
+            "XXXXXXXXXXXXXXXXXXX"
+    };
+
     public PacMan(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
@@ -28,6 +53,8 @@ public class PacMan extends JPanel {
 
         // Chargement des images
         loadImages();
+
+        System.out.println("Carte du jeu chargée : " + tileMap.length + " lignes x " + tileMap[0].length() + " colonnes");
     }
 
     private void loadImages() {
