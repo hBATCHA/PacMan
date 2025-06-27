79b1b90ffe0c2d1decb6c82f3d919deeae6920b5
Hashif Batcha <hashifbatcha@macbook-pro-de-hashif.home>
2025-06-28 01:24:09 +0200

    Étape 4 terminée - Chargement des images du jeu

--- src/PacMan.java
+++ src/PacMan.java
@@ -5,10 +5,47 @@ public class PacMan extends JPanel {
     private int boardWidth;
     private int boardHeight;
 
+    // Images du mur
+    private Image wallImage;
+
+    // Images des fantômes
+    private Image blueGhostImage;
+    private Image orangeGhostImage;
+    private Image pinkGhostImage;
+    private Image redGhostImage;
+
+    // Images de Pac-Man selon la direction
+    private Image pacmanUpImage;
+    private Image pacmanDownImage;
+    private Image pacmanLeftImage;
+    private Image pacmanRightImage;
+
     public PacMan(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
+
+        // Chargement des images
+        loadImages();
+    }
+
+    private void loadImages() {
+        // Chargement de l'image du mur
+        wallImage = new ImageIcon(getClass().getResource("/images/wall.png")).getImage();
+
+        // Chargement des images des fantômes
+        blueGhostImage = new ImageIcon(getClass().getResource("/images/blueGhost.png")).getImage();
+        orangeGhostImage = new ImageIcon(getClass().getResource("/images/orangeGhost.png")).getImage();
+        pinkGhostImage = new ImageIcon(getClass().getResource("/images/pinkGhost.png")).getImage();
+        redGhostImage = new ImageIcon(getClass().getResource("/images/redGhost.png")).getImage();
+
+        // Chargement des images de Pac-Man
+        pacmanUpImage = new ImageIcon(getClass().getResource("/images/pacmanUp.png")).getImage();
+        pacmanDownImage = new ImageIcon(getClass().getResource("/images/pacmanDown.png")).getImage();
+        pacmanLeftImage = new ImageIcon(getClass().getResource("/images/pacmanLeft.png")).getImage();
+        pacmanRightImage = new ImageIcon(getClass().getResource("/images/pacmanRight.png")).getImage();
+
+        System.out.println("Images chargées avec succès !");
     }
 }
